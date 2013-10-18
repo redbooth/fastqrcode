1810891ca52d8941d99974ec164a6f757e967709
Gregory Schlomoff <gregory.schlomoff@gmail.com>
2013-10-18 16:26:16 -0700

    Fixing README so that GitHub displays it nicely

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="fastqrcode",
     version="1.0",
     description="Fast and robust bindings for libqrencode",
+    long_description=open('README.md', 'rt').read(),
     url="https://github.com/aerofs/fastqrcode",
     author="Gregory Schlomoff",
     author_email="greg@aerofs.com",
